11c36b2c83a4fab19b36112186f1de70558d3181
Falk1n <warzekrom@gmail.com>
2024-05-17 07:24:52 -0300

    aside e home page
    aside adaptada com o bootstrap sem causar erro no codigo e algumas features da home page

--- codigo/home_page/assets/javascript/homejs.js
+++ codigo/home_page/assets/javascript/homejs.js
@@ -1,8 +1,35 @@
-const list = document.querySelectorAll(.list);
-function activelink(){
-    list.array.forEach((item) => 
-        item.classlist.remove('active'));
-        this.classlist.add('active');
+// script da troca de cores aside 
+const list = document.querySelectorAll('.list'); 
+function activeLink(){
+    list.forEach((item) => 
+    item.classList.remove('active'));
+    this.classList.add('active');
 }
-    list.forEach((item)=>
-    item.addEventListener('click',activelink));
+list.forEach((item)=>
+item.addEventListener('click', activeLink));
+// script ajax e XML 
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            const frame = document.getElementById("dataDisplay");
+
+            data.forEach(item => {
+                // Criar um elemento de cartão para cada objeto
+                let newDiv = document.createElement("div");
+                newDiv.className = "card";
+                newDiv.innerHTML = `
+                    <h2>${item.title}</h2>
+                    <p>${item.desc}</p>
+                    <p>Tags: ${item.tags}</p>
+                    <p>User: ${item.user}</p>
+                    <img src="${item.imgUrl}" alt="User Image" id="userimg">
+                    <button class="btn">View Details</button>`;
+                
+                // Adicionar o cartão ao elemento com o ID "loop"
+                frame.appendChild(newDiv);
+            });
+        })
+        .catch(error => console.error("Error fetching JSON data:", error));
+});
