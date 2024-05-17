21c1b8a531d2266b5c0fa5426ce5c1abdeb9857d
Falk1n <warzekrom@gmail.com>
2024-05-17 10:31:58 -0300

    Atualização home page
    Alteração no post e na aside

--- codigo/home_page/assets/javascript/homejs.js
+++ codigo/home_page/assets/javascript/homejs.js
@@ -18,18 +18,41 @@ document.addEventListener("DOMContentLoaded", function () {
             data.forEach(item => {
                 // Criar um elemento de cartão para cada objeto
                 let newDiv = document.createElement("div");
-                newDiv.className = "card";
+                newDiv.className = "card m-3";
                 newDiv.innerHTML = `
-                    <h2>${item.title}</h2>
-                    <p>${item.desc}</p>
-                    <p>Tags: ${item.tags}</p>
-                    <p>User: ${item.user}</p>
-                    <img src="${item.imgUrl}" alt="User Image" id="userimg">
-                    <button class="btn">View Details</button>`;
+                    <div class="row">
+
+                        <div class="col-2">
+                        <div class="mx-3 mt-3">
+                        <p class=>${item.views} Views</p>
+                        <p class=>${item.answer} Answer</p>
+                        <p class=>${item.vote} Vote</p>
+                        </div>
+                        
+                        </div>
+                        <div class="col-10">
+                            <div class="card">
+                            <div class="card-header"><h3>${item.title}</h3></div>
+                                <div class="card-body"><p>${item.desc}</p>
+                                    <div class="row ">
+                                        <div class="col-md-9">
+                                            <span class="badge rounded-pill bg-danger"><p class="m-0">${item.tags}</p></span>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <img src="${item.imgUrl}" alt="User Image" id="userimg" class="user-img">
+                                            <p class="user-name m-0" style="display:inline">${item.user}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>`;
                 
-                // Adicionar o cartão ao elemento com o ID "loop"
+                // Adicionar o cartão ao elemento com o ID "dataDisplay"
                 frame.appendChild(newDiv);
             });
         })
         .catch(error => console.error("Error fetching JSON data:", error));
 });
+
+
