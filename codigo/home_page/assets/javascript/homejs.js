cdb035fa3ac8670836a494b5d904e2b93f82d2b2
Falk1n <warzekrom@gmail.com>
2024-05-19 10:31:52 -0300

    Js barra de  pesquisa e filtro

--- codigo/home_page/assets/javascript/homejs.js
+++ codigo/home_page/assets/javascript/homejs.js
@@ -11,26 +11,53 @@ list.forEach((item) =>
 // script ajax e XML 
 document.addEventListener("DOMContentLoaded", function () {
     let data = [];
-    
+
     fetch('../json/data.json')
         .then(res => res.json())
         .then(jsonData => {
             data = jsonData;
             displayData(data);
+            tagsfiltro(data);
         })
         .catch(error => console.error("Error fetching JSON data:", error));
-    
-    function displayData(data) {
+
+    const input = document.getElementById('busca'); //Busca o elemento apartir do texto digitado e do seletor
+    input.addEventListener('input', () => {
+        const termopes = input.value.toLowerCase();
+        filtrardados(termopes, document.getElementById('Seletor').value);
+    });
+
+    const Seletor = document.getElementById('Seletor');//Filtra o  elemento somente com o seletor 
+    Seletor.addEventListener('change', () => {
+        filtrardados(input.value.toLowerCase(), Seletor.value);
+    });
+
+    function filtrardados(termopes, tagselecionada) { // função filtrar os dados do json apartir dos termos pesquisados e tags
+        let dadosfiltrado = data;
+        if (termopes) {
+            dadosfiltrado = dadosfiltrado.filter(item =>
+                item.title.toLowerCase().includes(termopes)
+            );
+        }
+        if (tagselecionada !== "filtro" && tagselecionada !== "") {
+            dadosfiltrado = dadosfiltrado.filter(item =>
+                item.tags.includes(tagselecionada)
+            );
+        }
+        displayData(dadosfiltrado);
+    }
+
+    function displayData(data) {  // função para colocar o  card na tela 
         const tela = document.getElementById("dataDisplay");
-        tela.innerHTML = ''; // Limpa a tela antes de renderizar os dados filtrados
-        
+        tela.innerHTML = '';  // Limpa o conteúdo existente
+
         data.forEach(item => {
             let newDiv = document.createElement("div");
             newDiv.className = "card col-md-12 m-3";
             newDiv.innerHTML = `
                 <div class="row">
-                    <div class="col-2" id="vote">
-                        <div class="ms-5 pt-4">
+                    <div class="col-2 text-center" id="vote">
+                        <div class="ms-0 pt-4">
                             <p><i class="bi bi-eye"></i> ${item.views}</p>
                             <p><i class="bi bi-chat-dots"></i> ${item.answer}</p>
                             <p><i class="bi bi-heart"></i> ${item.vote}</p>
@@ -38,9 +65,9 @@ document.addEventListener("DOMContentLoaded", function () {
                     </div>
                     <div class="col-10">
                         <div class="border">
-                            <div class="card-header "><h3>${item.title}</h3></div>
-                            <div class="card-body  ""><p>${item.desc}</p>
-                                <div class="row ">
+                            <div class="card-header"><h3>${item.title}</h3></div>
+                            <div class="card-body"><p>${item.desc}</p>
+                                <div class="row">
                                     <div class="col-md-9">
                                         ${item.tags.map(tag => `<span class="badge rounded-pill bg-danger p-2 m-1"><p class="m-0">${tag}</p></span>`).join('')}
                                     </div>
@@ -53,11 +80,23 @@ document.addEventListener("DOMContentLoaded", function () {
                         </div>
                     </div>
                 </div>`;
-        
             tela.appendChild(newDiv);
         });
     }
 
+    function tagsfiltro(data) { // função para pesquisar as tags do Json e colocar ela no seletor
+        const Seletor = document.getElementById('Seletor');
+        const tags = new Set();
+
+        data.forEach(item => {
+            item.tags.forEach(tag => tags.add(tag));
+        });
+
+        tags.forEach(tag => {
+            const opcao= document.createElement('option');
+            opcao.value = tag;
+            opcao.textContent = tag;
+            Seletor.appendChild(opcao);
+        });
+    }
 });
-
-
