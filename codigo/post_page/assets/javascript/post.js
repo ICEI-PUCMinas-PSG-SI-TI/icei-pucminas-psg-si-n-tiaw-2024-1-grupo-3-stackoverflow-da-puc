7dd7a75be3f785f1c338d8a4af69b3096d4a64a6
Caio Grossi <caiogrossi6@gmail.com>
2024-05-21 19:34:31 -0300

    post question

---
+++ codigo/post_page/assets/javascript/post.js
@@ -0,0 +1,34 @@
+const form = document.getElementById("post-question-form");
+
+form.addEventListener("submit", handlePostQuetion);
+
+function handlePostQuetion(event) {
+  event.preventDefault();
+
+  const title = document.getElementById("post-title").value;
+  const question = document.getElementById("question-post").value;
+  const tags = document.getElementById("tags-post").value;
+
+  const questions = localStorage.getItem("questions");
+
+  const question_json = {
+    title: title,
+    question: question,
+    tags: tags,
+  };
+
+  if (questions) {
+    const questions_array = JSON.parse(questions);
+
+    const id = questions_array.length + 1;
+
+    question_json.id = id;
+
+    questions_array.push(question_json);
+
+    localStorage.setItem("questions", JSON.stringify(questions_array));
+  } else {
+    question_json.id = 1;
+    localStorage.setItem("questions", JSON.stringify([question_json]));
+  }
+}
--- codigo/post_page/assets/javascript/postjs.js
+++
@@ -1,5 +0,0 @@
-const form = document.getElementById("post-question-form");
-
-form.addEventListener("submit", handlePostQuetion);
-
-function handlePostQuetion(event) {}
